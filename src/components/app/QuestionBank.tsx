fa71ee3dde25b0430a242443de2d5ac562c9e901
Mehedi Hasan <career.mehedihasan@gmail.com>
2025-07-18 19:07:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/app/QuestionBank.tsx
+++ src/components/app/QuestionBank.tsx
@@ -38,7 +38,7 @@ export default function QuestionBank({ questions }: QuestionBankProps) {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <Droppable droppableId="question-bank" isDropDisabled={true} isCombineEnabled={false}>
+        <Droppable droppableId="question-bank" isDropDisabled={true} isCombineEnabled={false} ignoreContainerClipping={false}>
           {(provided, snapshot) => (
             <ScrollArea 
                 ref={provided.innerRef}
